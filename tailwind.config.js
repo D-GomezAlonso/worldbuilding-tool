54540ddc138e6819b4ce7958c65413c63a7a3927
LambdaAbuser <cool.named.mail@gmail.com>
2024-09-28 17:01:02 +0200

    basic homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,14 +23,29 @@ module.exports = {
           "0%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(90deg)" },
         },
+        gradient: {
+          "0%": {
+            
+            "background-position": "0% 50%",
+          },
+          "50%": {
+            "background-position": "100% 50%",
+          },
+          "100%": {
+            "background-position": "0% 50%",
+          },
+        },
       },
       animation: {
         wiggle: "wiggle 0.5s ease-in-out",
+        gradient: "gradient 15s ease infinite",
       },
       boxShadow: {
         "image-panel": "0px 150px 5px -100px rgba(0, 0, 0, 0.6) inset",
       },
       backgroundImage: {
+        "home-gradient":
+          "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
         "radial-gradient":
           "linear-gradient(rgb(18, 18, 18) 0%, rgba(16, 16, 16, 0) 25%, rgba(24, 24, 24, 0) 75%, rgb(18, 18, 18) 100%)",
       },
