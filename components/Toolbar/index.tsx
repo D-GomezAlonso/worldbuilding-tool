a402941c717cca55ba79054604e746465f6c9660
LambdaAbuser <cool.named.mail@gmail.com>
2024-09-28 20:11:15 +0200

    Added new dropdown, not working

--- components/Toolbar/index.tsx
+++ components/Toolbar/index.tsx
@@ -10,36 +10,57 @@ import {
   DropdownMenu,
   DropdownTrigger,
 } from "@nextui-org/dropdown";
-import { BsFillPlusSquareFill } from "react-icons/bs";
 import { BsChevronDown } from "react-icons/bs";
 import { BsListUl } from "react-icons/bs";
 import { BsType } from "react-icons/bs";
 import { BsFillImageFill } from "react-icons/bs";
+import { ToolbarDropdown } from "./ToolbarDropdown";
+import { BsFileEarmarkTextFill } from "react-icons/bs";
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { BsFillPersonDashFill } from "react-icons/bs";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
+const panelDropdownItems = [
+  {
+    key: "list" as "list",
+    label: "List panel",
+    icon: BsListUl,
+  },
+  {
+    key: "text" as "text",
+    label: "Text panel",
+    icon: BsType,
+  },
+  {
+    key: "image" as "image",
+    label: "Image panel",
+    icon: BsFillImageFill,
+  },
+];
+
+const fileDropdownItems = [
+  {
+    key: "newCharacter" as "newCharacter",
+    label: "New Character",
+    icon: BsFillPersonPlusFill,
+  },
+  {
+    key: "renameCharacter" as "renameCharacter",
+    label: "Rename Character",
+    icon: BsFillPersonLinesFill,
+  },
+  {
+    key: "deleteCharacter" as "deleteCharacter",
+    label: "Delete Character",
+    icon: BsFillPersonDashFill,
+  },
+];
 
 export const Toolbar = ({
   addNewPanel,
 }: {
   addNewPanel: (panelType: "list" | "text" | "image") => void;
 }) => {
-  const panelDropdownItems = [
-    {
-      key: "list" as "list",
-      label: "List panel",
-      icon: BsListUl,
-    },
-    {
-      key: "text" as "text",
-      label: "Text panel",
-      icon: BsType,
-    },
-    {
-      key: "image" as "image",
-      label: "Image panel",
-      icon: BsFillImageFill,
-    },
-  ];
-
   return (
     <div className="flex flex-row dark:bg-gray-toolbar gap-2 items-center">
       <NextLink
@@ -56,23 +77,30 @@ export const Toolbar = ({
       </NextLink>
       <Divider orientation="vertical" className="max-h-7" />
 
+      <ToolbarDropdown
+        onClick={addNewPanel}
+        panelDropdownItems={panelDropdownItems}
+        triggerLabel="Add New Panel"
+      />
+      <Divider orientation="vertical" className="max-h-7" />
+
       <Dropdown>
         <DropdownTrigger>
           <Button variant="light" className="flex items-center ">
-            <BsFillPlusSquareFill />
-            Add New Panel
+            <BsFileEarmarkTextFill />
+            File
             <BsChevronDown />
           </Button>
         </DropdownTrigger>
         <DropdownMenu
           aria-label="Dynamic Actions"
-          items={panelDropdownItems}
+          items={fileDropdownItems}
           itemClasses={{
             title: "flex items-center gap-2",
           }}
         >
           {(item) => (
-            <DropdownItem key={item.key} onClick={() => addNewPanel(item.key)}>
+            <DropdownItem key={item.key}>
               <item.icon className="min-w-4 min-h-4" />
               <span>{item.label}</span>
             </DropdownItem>
